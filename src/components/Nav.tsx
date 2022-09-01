2e8b7cb430f2dbe5727bb82f5a9b49b31d822e92
Pellehakman <perhakman@outlook.com>
2022-09-01 15:25:19 +0200

    nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,21 @@
+// import { useNavigate } from 'react-router-dom';
+
+
 export default function Nav() {  
+
+    // const navigate = useNavigate();
+
+
+
     return (
-        <>
-        
-       <h1>hejd</h1>
-        
-        </>
+        <div>
+            {/* <nav>
+                <button className='' onClick={() => navigate('/home')}>HEM</button>
+                <button className='' onClick={() => navigate('/what')}>VÅRA DJUR</button>
+                <button className='' onClick={() => navigate('/what')}>DONERA</button>
+                <button className='' onClick={() => navigate('/what')}>OM OSS</button>            
+            </nav> */}
+        </div>
     )
 }
 
