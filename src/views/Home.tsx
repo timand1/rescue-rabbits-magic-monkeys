7d1b2ad7c8d486286d70da206b245274945c516f
Felix Stark <felix.b.stark@gmail.com>
2022-09-01 15:49:11 +0200

    routerized

---
+++ src/views/Home.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+
+import AnimalList from '../views/AnimalList'
+import './App.scss'
+
+function Home() {
+  
+
+  return (
+    <div className="Home">
+      {/* router */}
+      <AnimalList />
+    </div>
+  )
+}
+
+export default Home
