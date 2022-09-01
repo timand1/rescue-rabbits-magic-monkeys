347457772d33cf61937d24160c95e008599ef1ca
Pellehakman <perhakman@outlook.com>
2022-09-01 20:02:50 +0200

    nav+footer fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,22 +1,32 @@
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import logo from '../assets/LOGO.png'
 
 import '../styles/_nav.scss'
 
+
 export default function Nav() {  
 
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
-{/* <button className='' onClick={() => navigate('/home')}>HEM</button> */}
+
 
     return (
-        <div>
-            <nav>
-                <button className='nav-btn'>HEM</button>
-                <button className=''>VÅRA DJUR</button>
-                <button className=''>DONERA</button>
-                <button className=''>OM OSS</button>            
-            </nav>
-        </div>
+        <nav className='flex nav-container'>
+            <div className='flex nav'>
+                <figure className='flex nav-logo' style={{ backgroundImage: `url(${logo})`}}></figure>
+            
+            
+            
+                <button className='nav-btn nav-home' onClick={() => navigate('/')}><h1>HEM</h1></button>                
+                <button className='nav-btn nav-donate' onClick={() => navigate('/Donate')}><h1>DONERA</h1></button>
+                <button className='nav-btn nav-about' onClick={() => navigate('/About')}><h1>OM OSS</h1></button>  
+
+
+            </div>
+            
+                          
+           
+        </nav>
     )
 }
 
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -4,7 +4,7 @@ function About() {
 
     return (
         <div className="about">
-            
+            lets go!
         </div>
     )
 }
