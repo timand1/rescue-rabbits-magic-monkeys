4ca4166659233c38ff89324f56bdf6fa7ddad32b
Pellehakman <perhakman@outlook.com>
2022-09-02 10:16:15 +0200

    nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -11,22 +11,30 @@ export default function Nav() {
 
 
     return (
-        <nav className='flex nav-container'>
-            <div className='flex nav'>
+        <div className='flex nav-container'>
+
+            <nav className='flex nav'>
+
+
+            <div className='flex nav-left'>
                 <figure className='flex nav-logo' style={{ backgroundImage: `url(${logo})`}}></figure>
-            
-            
-            
-                <button className='nav-btn nav-home' onClick={() => navigate('/')}><h1>HEM</h1></button>                
+                <button className='nav-btn nav-home' onClick={() => navigate('/')}><h1>HEM</h1></button>  
+            </div>
+
+            <div className='flex nav-right'>
                 <button className='nav-btn nav-donate' onClick={() => navigate('/Donate')}><h1>DONERA</h1></button>
                 <button className='nav-btn nav-about' onClick={() => navigate('/About')}><h1>OM OSS</h1></button>  
-
-
             </div>
             
+                              
+                
+
+
+            
+            </nav>
                           
            
-        </nav>
+        </div>
     )
 }
 
