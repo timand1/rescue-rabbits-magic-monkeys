048ae96f65126d72b20453956445d5005117d2ef
Felix Stark <felix.b.stark@gmail.com>
2022-09-02 08:39:33 +0200

    about work

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,10 +1,16 @@
-
+import '../styles/About.scss'
 
 function About() {
 
     return (
-        <div className="about">
-            
+        <div className='view'>
+            <article className="about">
+                <h1>OM OSS</h1>
+                <section>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quos ea repellat amet voluptatum dolore culpa veniam, sit aliquam cupiditate alias tempora. Adipisci earum velit sunt fuga dignissimos fugit sequi?</p>
+                </section>
+
+            </article>
         </div>
     )
 }
