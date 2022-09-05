084fd7ec3c194fbd368d6cb08cf10cd4fa763556
Felix Stark <felix.b.stark@gmail.com>
2022-09-05 10:19:03 +0200

    confirmed progress

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -12,8 +12,7 @@ function About() {
                         <br /> <br />Webbappen ska presentera alla djur väntar på en ny ägare. 
                     Det behöver finnas en lista, där man kan se många, samt en vy där man kan titta närmare på ett djur. 
                     Man ska även kunna söka. Om användaren vill adoptera ett djur, ska det finnas ett formulär för att samla in relevant information. Djur som blivit "bokade" ska förstås inte gå att boka igen, men de kan fortfarande visas i listan</p>
-                    <figure style={{ backgroundImage: `url(${logo})` }}>
-                    </figure>
+                    <figure style={{ backgroundImage: `url(${logo})` }}></figure>
                 </section>
             </article>
 
--- src/views/Confirmed.tsx
+++ src/views/Confirmed.tsx
@@ -1,20 +1,28 @@
 // import '../styles/Confirmed.scss'
 import { Animal } from '../models/animal'
 import { User } from '../models/user'
+import '../styles/Confirmed.scss'
+import logo from '../assets/LOGO.png'
 
 interface ConfirmedProps {
     chosenAnimal: Animal | undefined;
     user: User;
   }
 function Confirmed(props: ConfirmedProps) {
+    console.log(props)
     console.log(props.user)
     console.log(props.chosenAnimal)
 
     return (
         <article className="confirmed">
-            <h1>TACK</h1>
-            <h2>För att du har räddat ett djur</h2>
-
+            <h2>Tack för att du har räddat ett djur!</h2>
+            <section>
+                <img src={props.chosenAnimal?.image} alt="" />
+                <p>{props.chosenAnimal?.name} är bokad till {props.user.fullName} och kan hämtas i våra lokaler i {props.chosenAnimal?.location}.</p>
+                <p>Bekräftelsenummer - genereras i databas?</p>
+                <p>Bekräftelse skickad till - {props.user.email}</p>
+                <figure style={{ backgroundImage: `url(${logo})` }}></figure>
+            </section>
         </article>
     )
 }
